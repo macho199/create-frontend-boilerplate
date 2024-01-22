9f9d61fea8b2c9aa6428a3c7ef9bd79992ae4360
Kwon Jong Seong <kjsdev@jobkorea.co.kr>
2024-01-22 14:54:50 +0900

    add modify webpack.config

--- create-react-webpack-typescript-app/webpack.config.js
+++ create-react-webpack-typescript-app/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.tsx",
@@ -17,11 +17,11 @@ module.exports = {
       {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        loader: "ts-loader"
+        loader: "ts-loader",
       },
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         test: /\.(jpg|jpeg|png|gif)$/,
@@ -37,5 +37,47 @@ module.exports = {
     new BundleAnalyzerPlugin(), // BundleAnalyzerPlugin 추가
     new MiniCssExtractPlugin(), // MiniCssExtractPlugin 추가
   ],
-  target: "web", // 브라우저에서 작동하는 코드로 설정
+};
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = (env) => {
+  return {
+    entry: "./src/index.tsx",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".jsx", ".js"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          loader: "ts-loader",
+        },
+        {
+          test: /\.css$/i,
+          use: [env.production ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(jpg|jpeg|png|gif)$/,
+          exclude: /node_modules/,
+          use: "file-loader",
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      }),
+      env.production ? new BundleAnalyzerPlugin() : '', // BundleAnalyzerPlugin 추가
+      env.production ? new MiniCssExtractPlugin() : '', // MiniCssExtractPlugin 추가
+    ],
+  };
 };
