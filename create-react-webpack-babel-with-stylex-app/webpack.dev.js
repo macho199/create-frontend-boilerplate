d796afb810898868883478c892b0614aa452abb0
Kwon Jong Seong <kjsdev@jobkorea.co.kr>
2024-01-22 14:43:54 +0900

    add browserslist and modify webpack.config

--- create-react-webpack-babel-with-stylex-app/webpack.dev.js
+++ create-react-webpack-babel-with-stylex-app/webpack.dev.js
@@ -2,14 +2,16 @@ const path = require("path");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.config.js");
 
-module.exports = merge(common, {
-  mode: "development",
-  devtool: "inline-source-map",
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
+module.exports = (env) => {
+  return merge(common(env), {
+    mode: "development",
+    devtool: "inline-source-map",
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "dist"),
+      },
+      port: 3000,
+      hot: true,
     },
-    port: 3000,
-    hot: true,
-  },
-});
+  });
+}
--- create-react-webpack-babel-with-stylex-app/webpack.prod.js
+++ create-react-webpack-babel-with-stylex-app/webpack.prod.js
@@ -3,13 +3,15 @@ const common = require("./webpack.config.js");
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = merge(common, {
-  mode: "production",
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new CssMinimizerPlugin(), // CSS 압축을 위한 플러그인 추가
-      new TerserPlugin(),
-    ],
-  },
-});
+module.exports = (env) => {
+  return merge(common(env), {
+    mode: "production",
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new CssMinimizerPlugin(), // CSS 압축을 위한 플러그인 추가
+        new TerserPlugin(),
+      ],
+    },
+  });
+}
