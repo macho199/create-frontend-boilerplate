2b2a9fb9ea6b998eecf224e34dc58692f94d0567
Kwon Jong Seong <kjsdev@jobkorea.co.kr>
2024-01-22 14:37:16 +0900

    add browserslist and modify webpack.config

--- create-react-webpack-babel-typescript-with-tailwindcss-and-twinmacro-app/webpack.config.js
+++ create-react-webpack-babel-typescript-with-tailwindcss-and-twinmacro-app/webpack.config.js
@@ -1,41 +1,42 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
-      },
-      {
-        test: /\.(jpg|jpeg|png|gif)$/,
-        exclude: /node_modules/,
-        use: "file-loader",
-      },
+module.exports = (env) => {
+  return {
+    entry: "./src/index.tsx",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".jsx", ".js"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          loader: "babel-loader",
+        },
+        {
+          test: /\.css$/i,
+          use: [env.production ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(jpg|jpeg|png|gif)$/,
+          exclude: /node_modules/,
+          use: "file-loader",
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      }),
+      env.production ? new BundleAnalyzerPlugin() : '', // BundleAnalyzerPlugin 추가
+      env.production ? new MiniCssExtractPlugin() : '', // MiniCssExtractPlugin 추가
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-    new BundleAnalyzerPlugin(), // BundleAnalyzerPlugin 추가
-    new MiniCssExtractPlugin(), // MiniCssExtractPlugin 추가
-  ],
-  target: "web", // 브라우저에서 작동하는 코드로 설정
+  };
 };
