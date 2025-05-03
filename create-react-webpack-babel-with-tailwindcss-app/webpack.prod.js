e3eda8bad1c1444aff4fdf1cdfbffac322cbfb3e
macho199 <macho199@gmail.com>
2025-05-04 03:21:50 +0900

    edit webpack.config

--- create-react-webpack-babel-with-tailwindcss-app/webpack.prod.js
+++ create-react-webpack-babel-with-tailwindcss-app/webpack.prod.js
@@ -2,16 +2,20 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.config.js');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = (env) => {
-  return merge(common(env), {
+module.exports = (env = {}) => {
+  return merge(common({ ...env, production: true }), {
     mode: 'production',
+    devtool: 'source-map',
     optimization: {
       minimize: true,
-      minimizer: [
-        new CssMinimizerPlugin(), // CSS 압축을 위한 플러그인 추가
-        new TerserPlugin(),
-      ],
+      minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
     },
+    performance: {
+      hints: false, // 번들 크기 경고 제거 (선택사항)
+    },
+    plugins: [new MiniCssExtractPlugin(), new BundleAnalyzerPlugin()],
   });
 };
